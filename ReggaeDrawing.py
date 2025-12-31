0b9e1908607c91aff93616893ee73bd29291c5fd
jan438 <jan438@users.noreply.github.com>
2025-12-31 10:12:04 +0100

    arc removed

--- ReggaeDrawing.py
+++ ReggaeDrawing.py
@@ -27,48 +27,6 @@ from reportlab.graphics.charts.textlabels import Label
 
 yellowbackground = "#ffde22"
 
-class MyArc(_Symbol):
-    def __init__(self, x, y):
-        self.x = x
-        self.y = y
-        self.fillColor = colors.blue
-        self.strokeColor = colors.purple
-        
-    def mycircle(self, x, y, radius, startdegree, smooth):
-        # sin(radians(30)) = 0.5
-        step = 90 / smooth
-        mcpoints = []
-        for i in range(smooth + 1):
-            mcpoints.append(x + cos(radians(startdegree + i * step)) * radius)
-            mcpoints.append(y + sin(radians(startdegree + i * step)) * radius)
-        mccurve = shapes.PolyLine(
-        points = mcpoints,
-                 strokeColor = self.strokeColor)
-        return mccurve
-
-    def draw(self):
-        g = shapes.Group()
-        logo1 = shapes.Polygon(
-        points=[self.x + 1.0, self.y + 2.0, self.x + 40.0, self.y + 30.0, self.x + 60.0, self.y + 70.0],
-               fillColor = self.fillColor,
-               strokeColor = self.strokeColor,
-               strokeWidth = 5)
-        g.add(logo1)
-        logo2 = shapes.PolyLine(
-        points=[self.x + 81.0, self.y + 102.0, self.x + 90.0, self.y + 43.0, self.x + 160.0, self.y + 170.0],
-               strokeColor = self.strokeColor,
-               strokeWidth = 5)
-        g.add(logo2)
-        mccurve1 = self.mycircle(300, 400, 100.0, 0, 36)
-        g.add(mccurve1)
-        mccurve2 = self.mycircle(300, 400, 100.0, 90, 36)
-        g.add(mccurve2)
-        mccurve3 = self.mycircle(300, 400, 100.0, 180, 36)
-        g.add(mccurve3)
-        mccurve4 = self.mycircle(300, 400, 100.0, 270, 36)
-        g.add(mccurve4)
-        return g
-
 class Hexagon(_Symbol):
     def __init__(self, x, y):
         self.x = x
@@ -153,6 +111,5 @@ d.add(transform_svg("Photos/PeterTosh.svg", 80, 80, avatars, avatars))
 h = Hexagon(115, 200)
 d.add(h)
 renderPDF.drawToFile(d, 'PDF/ReggaeDrawing.pdf')
-
 
 key = input("Wait")
