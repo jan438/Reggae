f5676acdff7e548c6a791e0175f9e7cea91ad618
jan438 <jan438@users.noreply.github.com>
2026-02-12 10:33:09 +0100

    2e kolom added

--- ReggaeLegends17A3.py
+++ ReggaeLegends17A3.py
@@ -199,7 +199,7 @@ d = Drawing(A3[1], A3[0])
 bgrect = shapes.Rect(0, 0, A3[1], A3[0], fillColor = background1, strokeColor = background1, strokeWidth = 0)
 d.add(bgrect)
 for i in range(len(legendsdata)):
-    if i == 3:
+    if i == 6:
         break
     drawLegendTriangle(d, i)
 d.add(transform_svg("Tribe of Judah.svg", 35.0, 450, 0.40, 0.40))  
@@ -208,7 +208,7 @@ d.add(transform_svg("cannabis.svg", 68.0, 436, 0.025, 0.025))
 d.add(transform_svg("cannabis.svg", 88.0, 436, 0.025, 0.025))
 d.add(transform_svg("cannabis.svg", 108.0, 436, 0.025, 0.025))
 for i in range(len(legendsdata)):
-    if i == 3:
+    if i == 6:
         break
     drawLegendLines(d, i)
 
